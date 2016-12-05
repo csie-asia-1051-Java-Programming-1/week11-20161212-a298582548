3c198b4b2dbb0f71e15b14e800be4ed38c4fe4e6
a298582548 <105021010@live.asia.edu.tw>
2016-12-05 16:27:40 +0800

    pp

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,10 +1,14 @@
 package ex;
 /*
  * Date: 2016/12/12
- * Author: 105021***  ???
+ * Author: 105021010 邱信喆
  */
+import java.util.Scanner;
 public class ex02 {
 	public static void main(String[] args) {
-
+	Scanner scn = new Scanner(System.in);
+	while(true){
+		
+	}
 	}
 }
